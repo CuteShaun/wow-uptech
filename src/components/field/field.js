2f3f26b31b18f5a698f9fec02bc40e09453f1e23
CuteShaun <andrewtsapko@gmail.com>
2020-01-20 13:43:43 +0200

    Add field component

--- src/components/field/field.js
+++ src/components/field/field.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Field = ({ type, id, value }) => {
+const Field = ({ type, id, value, labelText }) => {
   return (
     <>
-      <label htmlFor={id}></label>
+      <label htmlFor={id}>{labelText}</label>
       <input type={type} id={id} value={value}></input>
     </>
   );
